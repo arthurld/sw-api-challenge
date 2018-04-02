80334eb50994e8ce3ab6b02bd7ee6989ddefce42
Arthur Domingues <arth.domingues@gmail.com>
2018-04-02 19:15:49 -0300

    reset styles added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Home from './pages/home'
 import { AppContainer } from 'react-hot-loader'
+import './reset.styl'
 
 const render = Component => {
     ReactDOM.render(
